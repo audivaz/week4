986128a0d32cdfd49bc35950be5e51b0cb05eb5f
audivaz <audivaz3@gmail.com>
2020-10-13 08:43:38 -0400

    added html and javascript

---
+++ app1.js
@@ -0,0 +1,39 @@
+var message = 'Hello world!';
+    console.log(message);
+
+var warning = 'This is a warning';  
+    console.log(warning);
+
+var error = 'Error! System critical';
+    console.log(error);
+
+
+
+var var1 = 'Crazy';
+    console.log(var1);
+
+console.log('');
+
+var var2 = '88';
+    console.log(var2);
+
+console.log(var1 + ' ' + String(var2)); // Crazy 88, from the movie Kill Bill
+
+/* This is a multiline comment.
+All the text written here will not appear in the console 
+or in the browser */
+
+var var3 = 'string';
+    console.log(var3);
+
+var3 = true;
+    console.log(var3);
+
+var1 = 5;
+    console.log(var1);
+
+var2 = '5';
+    console.log(var2);
+
+var var4 = var1 + (var2);
+    console.log(var4);
