67782b8b5063f61d9074ed8f993cf43e3a08d69b
audivaz <audivaz3@gmail.com>
2020-10-14 14:58:38 -0400

    added javascprit basic fundamentals

---
+++ app2.js
@@ -0,0 +1,61 @@
+//--- step 5
+var num1 = prompt('Enter a number');
+    console.log(num1);
+
+//--- step 6
+var num2 = prompt('Enter in another number');
+    console.log(num2);
+
+//--- step 7
+alert('You chose... wisely');
+
+
+//--- step 8
+console.log(Math.floor(Math.random() * 3) + 1);
+
+//--- step 9
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f'];
+    console.log(alphabet);
+
+//--- step 10
+alphabet.pop('f');
+    console.log(alphabet);
+
+//--- step 11
+alphabet.push('g');
+    console.log(alphabet);
+
+//--- step 12
+alphabet.shift();
+    console.log(alphabet);
+
+//--- step 13
+alphabet.unshift(1)
+    console.log(alphabet);
+
+//--- step 14
+
+
+
+//--- step 15
+alphabet.splice(2, 3, 'cat', 'dog', 'rabbit');
+    console.log(alphabet);
+
+//--- step 16
+
+var copied = alphabet.slice();
+    console.log(copied);
+
+//--- step 17
+
+console.log(alphabet[3]);
+
+
+
+
+
+
+
+
+
+
