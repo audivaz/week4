f5d031d6fb3101df6fbe0ffca04e2612b6b28fa8
audivaz <audivaz3@gmail.com>
2020-10-16 22:59:11 -0400

    working on console game

--- app3.js
+++ app3.js
@@ -5,7 +5,7 @@
 
 var game = new Object();
     win = 0;
-    loss= 0;
+    loss = 0;
 
    // console.log(win);
 
@@ -27,28 +27,38 @@ var random2 = (Math.floor(Math.random() * 3) + 0);
 //------ step 7
 
 var bot1 = choices[random1];
-
+    console.log(bot1);
 var bot2 = choices[random2];
-    console.log(random1);
-
-//------ step 8
-
-if (bot1 > 1) {
-    console.log('win')
-} else {
-    console.log('loss')
-}
-//------ step 9 
-
-var results = ' ';
-    console.log(results);
+    console.log(bot2);
 
 
 
+// figure out what bot1 picked
 
-//------ step 10
+if(random1 === random2) {
+    console.log('tie');
+} 
 
+// if (random1 === 'rock' + random2 === 'paper') {
+//     console.log('paper wins');
+// } 
 
-if('win' > 0) {
-    console.log(results + 'beats!');
-}
+// if (random1 === 'rock' + random2 === 'scissors') {
+//     console.log('rock wins');
+// } 
+
+// if  (random1 === 'paper' + random2 === 'rock') {
+//     console.log('paper wins');
+// } 
+
+// if (random1 === 'paper' + random2 === 'scissor') {
+//     console.log('scissor wins');
+// } 
+
+// if (random1 === 'scissor' + random2 === 'rock') {
+//     console.log('rock wins');
+// } 
+
+// if (random1 === 'scissor' + random2 === 'paper') {
+//     console.log('scissor wins');
+// }
